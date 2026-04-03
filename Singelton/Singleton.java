8743721e4fae016a5f96e67a14b767d064f3b60e
Osagani31 <naonperera@students.nsbm.ac.lk>
2026-04-03 08:41:01 +0530

    Learnt Singelton design pattern core concepts

---
+++ Singelton/Singleton.java
@@ -0,0 +1,19 @@
+public class Singleton {
+
+private static Singleton singleton;
+
+private Singleton(){
+
+}
+
+public static Singleton getInstance(){
+    if (singleton==null){
+        return singleton=new Singleton();
+    }
+    return singleton;
+
+}
+
+
+
+}
